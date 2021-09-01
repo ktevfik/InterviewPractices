0d832654b2de422bc9785d209385254c86603144
Tevfik Kadan <mtevfik41@gmail.com>
2021-09-01 22:08:02 +0300

    Exercise 4

--- src/DataStructuresAndAlgorithmsInJava/chapter1/Exercise4.java
+++ src/DataStructuresAndAlgorithmsInJava/chapter1/Exercise4.java
@@ -4,7 +4,7 @@
  */
 
 package DataStructuresAndAlgorithmsInJava.chapter1;
-import java.util.*;
+
 public class Exercise4 {
     public static void main(String[] args) {
         System.out.println(isEven(4));
@@ -12,24 +12,28 @@ public class Exercise4 {
         System.out.println(oddSum(35));
         System.out.println(sumPow(32));
     }
+
     public static boolean isEven(int n) {
         return n % 2 == 0;
     }
+
     public static int sum(int n) {
         int result = 0;
-        for(int i = 1;i <= n;i++) result += i;
+        for (int i = 1; i <= n; i++) result += i;
         return result;
     }
+
     public static int oddSum(int n) {
         int result = 0;
-        for(int i = 1;i <= n;i++) {
-            if(i % 2 == 1) result += i;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) result += i;
         }
         return result;
     }
+
     public static int sumPow(int n) {
         int result = 0;
-        for(int i = 1;i <= n;i++) result += (i * i);
+        for (int i = 1; i <= n; i++) result += (i * i);
         return result;
     }
 }
